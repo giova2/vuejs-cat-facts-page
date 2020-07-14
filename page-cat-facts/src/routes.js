b88b0caee1f9ccc6bcb9e8b9728739cf31b25399
franco giovannangelo <fcgiova@gmail.com>
2020-07-14 18:07:18 -0300

    try to fix incongruency with routes in prod and dev

--- page-cat-facts/src/routes.js
+++ page-cat-facts/src/routes.js
@@ -1,14 +1,21 @@
 import Facts from "./components/Facts.vue";
 import Fact from "./components/Fact.vue";
+import * as env from "./environment.js";
 
 export default [
   {
-    path: "/",
+    path: `${
+      process.env.NODE_ENV === "production" ? "/vuejs-cat-facts-page/" : "/"
+    }`,
     name: "facts",
     component: Facts
   },
   {
-    path: `/fact/:id`,
+    path: `${
+      process.env.NODE_ENV === "production"
+        ? "/vuejs-cat-facts-page/fact/:id"
+        : "/fact/:id"
+    }`,
     name: "fact",
     component: Fact
   },
